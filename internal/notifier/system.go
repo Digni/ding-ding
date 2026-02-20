668590c4bdfe984331348a46c8c12a1de54f28e4
Fabian Freimueller <fabian@freimueller.email>
2026-02-20 08:55:18 +0100

    fix(notifier): escape percent signs in Windows toast notifications
    User input containing % (e.g. "Build 100% done") was interpreted as
    fmt.Sprintf format verbs, corrupting the PowerShell XML template.
    Escape % as %% after XML escaping, before Sprintf interpolation.

--- internal/notifier/system.go
+++ internal/notifier/system.go
@@ -24,6 +24,8 @@ func systemNotify(title, body string) error {
 		script := `display notification (item 1 of argv) with title (item 2 of argv)`
 		return exec.Command("osascript", "-e", "on run argv", "-e", script, "-e", "end run", body, title).Run()
 	case "windows":
+		escTitle := strings.ReplaceAll(xmlEscape(title), "%", "%%")
+		escBody := strings.ReplaceAll(xmlEscape(body), "%", "%%")
 		ps := fmt.Sprintf(`
 [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
 [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
@@ -32,7 +34,7 @@ $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
 $xml.LoadXml($template)
 $toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
 [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("ding-ding").Show($toast)
-`, xmlEscape(title), xmlEscape(body))
+`, escTitle, escBody)
 		return exec.Command("powershell", "-Command", ps).Run()
 	default:
 		return fmt.Errorf("unsupported platform: %s", runtime.GOOS)
